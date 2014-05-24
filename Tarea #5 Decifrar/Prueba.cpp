22d4e7e4c9df82e1156f33267293ccb5475e3201
Daniel Gutierrez <conan_022@hotmail.com>
2014-05-23 20:45:52 -0400

    Archivo txt de salida

--- Tarea #5 Decifrar/Prueba.cpp
+++ Tarea #5 Decifrar/Prueba.cpp
@@ -5,13 +5,15 @@ using namespace std;
 int main () {
  fstream ParteUno, ParteDos;
  char LetraUno, LetraDos;
-
+ ofstream Salida("TextoSalida.txt");
  ParteUno.open ("p1.txt", ios::in);
  ParteDos.open ("p2.txt", ios::in);
  if (ParteUno.is_open()&&ParteUno.is_open()) {
  while (! ParteUno.eof() ) {
  ParteUno >> LetraUno;
  ParteDos >> LetraDos;
+ Salida << LetraUno;
+ Salida << LetraDos;
  cout << LetraUno << " ";
  cout << LetraDos << " ";
  }
