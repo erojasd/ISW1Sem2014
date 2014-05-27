43da5f6d32b1c9da8db90c18ab9e09e12a796cc1
Daniel Gutierrez <conan_022@hotmail.com>
2014-05-26 21:32:05 -0400

    Update xml.cpp
    Conexion con la WEB mediante Curl

--- Tarea #6 XML/xml.cpp
+++ Tarea #6 XML/xml.cpp
@@ -1,3 +1,16 @@
+/*
+DATOS DES PROGRAMA
+NOMBRE          : xml.cpp correspondiente a la tarea6 de Ingenieria de Software
+FECHA ENTREGA   : Lunes 26 Mayo
+UTEM
+
+Programa probado y testeado en un notebook HP con Maquina Virtual LinuxMInt 15 | 64 bit | 2GB de RAM
+
+Advertencia:
+Para compilar usar el comando   : g++ xml.cpp -lcurl -o xml
+Para Usar el programa           : ./xml -d  || ./xml -v
+*/
+
 /*Bibliotecas estandar utilizadas*/
 #include <iostream>
 #include <cstring>
@@ -7,11 +20,13 @@
 #include <cstdlib>
 #include <fstream>
 #include <string.h>
+#include <curl/curl.h>
 
-/*Para trabajar con XML se prueba la libreria pugixml */
+/*Para trabajar con XML se prueba la libreria pugixml se uso como prueba para parsear
 #include "src/pugiconfig.hpp"
 #include "src/pugixml.hpp"
 #include "src/pugixml.cpp"
+*/
 
 /*Constantes*/
 #define _TIME_
@@ -19,31 +34,29 @@
 
 using namespace std;
 
-/*Variables Globales*/
- // se utiliza en funcion para pasar a mayuscula
-char Pos2[N];
-
-// Host de internet donde reside el mensaje
-const char *host = "http://sebastian.cl/isw-rest/api/mensajeCifrado";
-
-/*Funcion para pasar a mayuscula el segundo parametro de entrada*/
-void PasarMayuscula(void)
+/*Funcion para*/
+static size_t WriteCallback(void *contents, size_t size, size_t nmemb, void *userp)
 {
-    int i;
-    for(i = 0; i< N; i++) Pos2[i]=toupper(Pos2[i]);
+    ((string*)userp)->append((char*)contents, size * nmemb);
+    return size * nmemb;
 }
 
 /*Funcion principal y cuerpo del programa*/
 int main(int argc, char *argv[])
 {
 /*Variables de la funcion principal*/
-int entrada = 0, i = 0;
-bool BanderaDePaso = false;
+int entrada = 0, i, PosMensaje, PivoteMensaje;
 time_t tiempo = time(0);
 struct tm *tlocal = localtime(&tiempo);
 char SalidaFecha[128], Fila[100];
+string RespuestaWEB;
+
 // Mensaje a desencriptar
-char Mensaje [N];
+char Mensaje [30];
+
+/*Se crean las instancias de tipo CURL para la conexion con la WEB*/
+CURL *curl;
+CURLcode res;
 
 /*Se define cual es el parametro de entrada*/
 for(i=0;i< argc;i++)
@@ -58,39 +71,52 @@ for(i=0;i< argc;i++)
 "http://sebastian.cl/isw-rest/api/mensajeCifrado"*/
 if (entrada == 1)
 {
-    pugi::xml_document doc;
-    if (!doc.load_file("tarea.xml"))
-	{
-		cerr << "Error al cargar el documento XML." << endl;
-		return 0;
-	}
-	// Creamos un objeto nodo
-	pugi::xml_node root_node;
-	// Le asignamos el nodo principal comprobando que sea correcto sino no sigue el programa
-	if (!(root_node = doc.child("mensaje")))
-	{
-		cerr << "El documento no es un mapa valido." << endl;
-		return 0;
-	}
-    /*Se recorre la estructura del archivo, teniendo en cuenta que este no varia en el tiempo*/
-	for (pugi::xml_node node = root_node.first_child(); node; node = node.next_sibling())
-	{
-		string node_name = node.name();
-        /*Si encontramos la etiqueta mensaje se guarda su contenido en una variable llamada Mensaje para su posterior
-        Desencriptacion*/
-		if (node_name == "mensaje")
-		{
-		    strcpy(Mensaje, node.child_value());
-		    cout << "El mensaje obtenido fue: ";
-		    cout << Mensaje << endl;
-		    BanderaDePaso = true;
-		}
-	}
-	if (!BanderaDePaso)
-    {
-        cout << "No se encontro la etiqueta Mensaje" << endl;
-        return 0;
+
+/*________________________Bloque para conectar con la WEB_______________________________________________*/
+/*
+Comentario: Por mas que se intento mostrar un arbol del archivo XML de la web http://sebastian.cl/isw-rest/api/mensajeCifrado
+no se pudo, pero si se logro recopilar el contenido explicito de los nodo hijo. Pero para archivos de hiperTexto mostraba toda la
+estructura de arbol. Claramente no es la opcion mas valida para cuando se hable uso de memoria, pero como es una estructura pequeña
+nos parecio acertado usar este metodo para resolver la tarea
+*/
+
+   curl = curl_easy_init();
+   if(curl)
+   {
+       curl_easy_setopt(curl, CURLOPT_URL, "http://sebastian.cl/isw-rest/api/mensajeCifrado");
+       curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteCallback);
+       curl_easy_setopt(curl, CURLOPT_WRITEDATA, &RespuestaWEB);
+       res = curl_easy_perform(curl);
+       curl_easy_cleanup(curl);
+       PosMensaje = RespuestaWEB.find("mensaje");
+       /*Condicion de salida en caso de no encontrar la etiqueta mensaje*/
+       if(PosMensaje > RespuestaWEB.length())
+       {
+            cout << "No se encontró el mensaje" << endl;
+            return 0;
+       }
+       /* Se mueve 10 posiciones en el String de entrada para llegar justo donde comienza el codigo*/
+       PosMensaje = PosMensaje + 10;
+       PivoteMensaje = 0;
+       cout << "Los Datos obtenidos de la WEB fueron: ";
+       cout << RespuestaWEB << endl;
+       /* While utilizado para parsear el mensaje, siempre considerando que este mensaje estara
+       a 10 posiciones del inicio de la etiqueta "mensaje"*/
+       while(PivoteMensaje < 12)
+       {
+           Mensaje[PivoteMensaje] = RespuestaWEB[PosMensaje];
+           PosMensaje++;
+           PivoteMensaje++;
+       }
+       cout << "" <<endl;
+       cout << "EL mensaje parseado fue: ";
+       cout << Mensaje << endl;
+
+
     }
+/*________________________________Fin de Bloque para conectar con la WEB_______________________________________*/
+
+
 }
 
 /*Si el programa se ejecuta opción “-v” debe mostrar en pantalla (al menos
@@ -123,3 +149,42 @@ else
 return 0;
 }
 
+
+/*________________________________________Codigo Inutilizado___________________________________________________*/
+
+/*________________________________Parseo Local del archivo tarea.xml_______________________________________
+
+    pugi::xml_document doc;
+    if (!doc.load_file("tarea.xml"))
+	{
+		cerr << "Error al cargar el documento XML." << endl;
+		return 0;
+	}
+	// Creamos un objeto nodo
+	pugi::xml_node root_node;
+	// Le asignamos el nodo principal comprobando que sea correcto sino no sigue el programa
+	if (!(root_node = doc.child("mensaje")))
+	{
+		cerr << "El documento no es un mapa valido." << endl;
+		return 0;
+	}
+    Se recorre la estructura del archivo, teniendo en cuenta que este no varia en el tiempo
+	for (pugi::xml_node node = root_node.first_child(); node; node = node.next_sibling())
+	{
+		string node_name = node.name();
+        Si encontramos la etiqueta mensaje se guarda su contenido en una variable llamada Mensaje para su posterior
+        Desencriptacion
+		if (node_name == "mensaje")
+		{
+		    strcpy(Mensaje, node.child_value());
+		    cout << "El mensaje obtenido fue: ";
+		    cout << Mensaje << endl;
+		    BanderaDePaso = true;
+		}
+	}
+	if (!BanderaDePaso)
+    {
+        cout << "No se encontro la etiqueta Mensaje" << endl;
+        return 0;
+    }
+________________________________Fin de Parseo Local del archivo tarea.xml_______________________________________*/
